3a6916f46a8752b9a4dc0f6913858f22b699136d
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2016-09-10 13:15:33 +0300

    Exclude prompt from the result (#10)
    *  #10 char
    
    *  #10 Implement left, kill
    
    *  #10 left/right
    
    *  #10 delete
    
    *  #10 ctrl+u
    
    *  #10 Exclude prompt

--- test/rline/application_test.rb
+++ test/rline/application_test.rb
@@ -2,7 +2,19 @@ require 'test_helper'
 
 class RLine::ApplicationTest < TestCase
   def subject
-    @subject ||= RLine::Application.new
+    @subject ||= RLine::Application.new(screen)
+  end
+
+  def screen
+    @screen ||= Minitest::Mock.new
+  end
+
+  def ctrl_(char)
+    RLine::ControlCharacter.new((char.ord - '@'.ord).chr)
+  end
+
+  def teardown
+    screen.verify
   end
 
   def call(*args)
@@ -15,5 +27,64 @@ class RLine::ApplicationTest < TestCase
 
   def test_eof
     assert_equal RLine::Exit.new, call(RLine::EOF.new)
+  end
+
+  def test_char
+    char = ('a'..'z').to_a.sample
+    result = rand
+    screen.expect(:print_char, result, [char])
+
+    assert_equal result, call(RLine::Character.new(char))
+  end
+
+  def test_backspace
+    result1 = rand
+    result2 = rand
+
+    screen.expect(:left, result1)
+    screen.expect(:kill, result2)
+
+    assert_equal [result1, result2], call(RLine::Backspace.new)
+  end
+
+  def test_left
+    result = rand
+
+    screen.expect(:left, result)
+
+    assert_equal result, call(RLine::ArrowLeft.new)
+  end
+
+  def test_right
+    result = rand
+
+    screen.expect(:right, result)
+
+    assert_equal result, call(RLine::ArrowRight.new)
+  end
+
+  def test_delete
+    result = rand
+
+    screen.expect(:kill, result)
+
+    assert_equal result, call(RLine::Delete.new)
+  end
+
+  def test_ctrl_u
+    left_result = rand
+    kill_result = rand
+    cursor = 5
+
+    2.times do
+      screen.expect(:cursor, cursor)
+    end
+
+    cursor.times do
+      screen.expect(:left, left_result)
+      screen.expect(:kill, kill_result)
+    end
+
+    assert_equal [left_result, kill_result] * cursor, call(ctrl_('u'))
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,6 @@ require 'minitest/autorun' unless ENV.has_key?('GUARD')
 require 'minitest/reporters'
 
 require 'rline'
-require 'pty'
 
 Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
 
